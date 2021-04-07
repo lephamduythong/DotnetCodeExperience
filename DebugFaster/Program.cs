6329d732caffafb5bbd25a294bc9b06ad04f698f
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2021-04-07 22:07:12 +0700

    DebugFaster: Improve

--- DebugFaster/Program.cs
+++ DebugFaster/Program.cs
@@ -4,46 +4,49 @@ using System.Text.RegularExpressions;
 
 namespace DebugFaster
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      // Change these settings
-      int selectionCode = 1;
-      int startIndex = 0;
+        static void Main(string[] args)
+        {
+            // Change these settings
+            int selectionCode = int.Parse(args[0]);
+            int startIndex = 0;
 
-      // Init params
+            string startWithPattern = string.Empty;
+            string pattern = "DEBUG_DEBUG";
+            string exceptPattern = string.Empty;
+            string initReplaceText = string.Empty;
+            string text = File.ReadAllText("input.txt");
+            Regex regex = new Regex(pattern);
+			startWithPattern = ";";
+            initReplaceText = "; " + pattern;
+            text = text.Replace(startWithPattern, initReplaceText);
+            text = text.Replace("\\", string.Empty);
+			System.Console.WriteLine(text);
+			MatchCollection mc = Regex.Matches(text, pattern);
 
-      string startWith = string.Empty;
-      string pattern = string.Empty;
-      string initReplaceText = string.Empty;
-      string text = File.ReadAllText("input.txt");
-
-      switch (selectionCode)
-      {
-        // After ";" Javascript
-        case 1:
-          // Config
-          startWith = ";";
-          pattern = "console.log\\(\"\\[-___-\\]\"\\);";
-          initReplaceText = "; " + pattern;
-          // Process
-          text = text.Replace(startWith, initReplaceText);
-          text = text.Replace("\\", string.Empty);
-          Regex regex = new Regex(pattern);
-          MatchCollection mc = Regex.Matches(text, pattern);
-          for (int i = startIndex; i < mc.Count; i++)
-          {
-            text = regex.Replace(text, $"console.log\\(\"\\[- {i} -\\]\"\\);", 1);
-          }
-          text = text.Replace("\\", string.Empty);
-          break;
-        // After ";" C#
-        case 2:
-          break;
-      }
-      File.WriteAllText("output.txt", text);
-      Console.WriteLine("DONE");
+            switch (selectionCode)
+            {
+				// Javascript
+                case 1:
+                    for (int i = startIndex; i < mc.Count; i++)
+                    {
+						System.Console.WriteLine(i);
+                        text = regex.Replace(text, $"console.log\\(\"\\[- {i} -\\]\"\\);", 1);
+                    }
+                    text = text.Replace("\\", string.Empty);
+                    break;
+                // C#
+                case 2:
+                    for (int i = startIndex; i < mc.Count; i++)
+                    {
+                        text = regex.Replace(text, $"Debug.WriteLine\\(\"\\[- {i} -\\]\"\\);", 1);
+                    }
+                    break;
+            }
+			text = text.Replace("\\", string.Empty);
+            File.WriteAllText("output.txt", text);
+            Console.WriteLine("DONE");
+        }
     }
-  }
 }
