c1c44c7e593f7d9510bd48c6ff09d45d1a7b7782
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2021-04-14 21:49:41 +0700

    DebugFaster: Improve for c#

--- DebugFaster/Program.cs
+++ DebugFaster/Program.cs
@@ -10,8 +10,8 @@ namespace DebugFaster
         {
             // Change these settings
             int selectionCode = int.Parse(args[0]);
+            // int selectionCode = 2;
             int startIndex = 0;
-            string saltStr = "";
 
             string startWithPattern = string.Empty;
             string pattern = "DEBUG_DEBUG";
@@ -19,33 +19,15 @@ namespace DebugFaster
             string initReplaceText = string.Empty;
             string text = File.ReadAllText("input.txt");
             Regex regex = new Regex(pattern);
-			startWithPattern = ";";
+            startWithPattern = ";";
             initReplaceText = "; " + pattern;
             text = text.Replace(startWithPattern, initReplaceText);
             text = text.Replace("\\", string.Empty);
-			MatchCollection mc = Regex.Matches(text, pattern);
-
-            // Remove line that contains unreachable code
-            var listLines = text.Split('\n');
-            for (var i = 0; i < listLines.Length; i++)
-            {   
-                if (listLines[i].Contains("return") ||
-                    listLines[i].Contains("break") ||
-                    listLines[i].Contains("continue"))
-                {
-                    listLines[i] = listLines[i].Replace(" DEBUG_DEBUG", "");
-                }
-            }
-            // Combine to text
-            text = "";
-            foreach (var line in listLines)
-            {
-                text += line;
-            }
+            MatchCollection mc = Regex.Matches(text, pattern);
 
             switch (selectionCode)
             {
-				// Javascript
+                // Javascript
                 case 1:
                     for (int i = startIndex; i < mc.Count; i++)
                     {
@@ -55,13 +37,56 @@ namespace DebugFaster
                     break;
                 // C#
                 case 2:
+                    // Remove lines that contain unreachable code
+                    var listLines = text.Split('\n');
+                    for (var i = 0; i < listLines.Length; i++)
+                    {
+                        string type = null;
+                        bool isSingle = false;
+
+                        if (listLines[i].Contains("return"))
+                        {
+                            type = "return";
+                        }
+                        else if (listLines[i].Contains("break"))
+                        {
+                            type = "break";
+                        }
+                        else if (listLines[i].Contains("continue"))
+                        {
+                            type = "continue";
+                        }
+
+                        if (i >= 1)
+                        {
+                            var test = listLines[i - 1].Trim();
+                            if (test == "{")
+                            {
+                                isSingle = true;
+                            }
+                        }
+
+                        if (type != null && isSingle == true)
+                        {
+                            listLines[i] = listLines[i].Replace(type, "DEBUG_DEBUG_FIX " + type);
+                            listLines[i] = listLines[i].Replace(" DEBUG_DEBUG", "");
+                            listLines[i] = listLines[i].Replace("DEBUG_DEBUG_FIX", "DEBUG_DEBUG");
+                        }
+                    }
+                    text = "";
+                    foreach (var line in listLines)
+                    {
+                        text += line;
+                    }
+
+                    // Replace to debug code
                     for (int i = startIndex; i < mc.Count; i++)
                     {
                         text = regex.Replace(text, $"System.Diagnostics.Debug.WriteLine(\"-------------{i}-------------\");", 1);
                     }
                     break;
             }
-			text = text.Replace("\\", string.Empty);
+            text = text.Replace("\\", string.Empty);
             File.WriteAllText("output.txt", text);
             Console.WriteLine("DONE");
         }
