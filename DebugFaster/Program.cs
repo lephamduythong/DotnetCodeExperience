aa88647acc9ba24b5216bf1f11447304481e8803
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2021-04-14 20:51:26 +0700

    DebugFaster: update

--- DebugFaster/Program.cs
+++ DebugFaster/Program.cs
@@ -11,6 +11,7 @@ namespace DebugFaster
             // Change these settings
             int selectionCode = int.Parse(args[0]);
             int startIndex = 0;
+            string saltStr = "";
 
             string startWithPattern = string.Empty;
             string pattern = "DEBUG_DEBUG";
@@ -22,8 +23,25 @@ namespace DebugFaster
             initReplaceText = "; " + pattern;
             text = text.Replace(startWithPattern, initReplaceText);
             text = text.Replace("\\", string.Empty);
-			System.Console.WriteLine(text);
 			MatchCollection mc = Regex.Matches(text, pattern);
+
+            // Remove line that contains unreachable code
+            var listLines = text.Split('\n');
+            for (var i = 0; i < listLines.Length; i++)
+            {   
+                if (listLines[i].Contains("return") ||
+                    listLines[i].Contains("break") ||
+                    listLines[i].Contains("continue"))
+                {
+                    listLines[i] = listLines[i].Replace(" DEBUG_DEBUG", "");
+                }
+            }
+            // Combine to text
+            text = "";
+            foreach (var line in listLines)
+            {
+                text += line;
+            }
 
             switch (selectionCode)
             {
@@ -31,7 +49,6 @@ namespace DebugFaster
                 case 1:
                     for (int i = startIndex; i < mc.Count; i++)
                     {
-						System.Console.WriteLine(i);
                         text = regex.Replace(text, $"console.log\\(\"\\[- {i} -\\]\"\\);", 1);
                     }
                     text = text.Replace("\\", string.Empty);
@@ -40,7 +57,7 @@ namespace DebugFaster
                 case 2:
                     for (int i = startIndex; i < mc.Count; i++)
                     {
-                        text = regex.Replace(text, $"Debug.WriteLine\\(\"\\[- {i} -\\]\"\\);", 1);
+                        text = regex.Replace(text, $"System.Diagnostics.Debug.WriteLine(\"-------------{i}-------------\");", 1);
                     }
                     break;
             }
